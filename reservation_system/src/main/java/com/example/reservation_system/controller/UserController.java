cf7a3ddfb5e80601db12953c35ee7539bd66f2f0
Магомед <magomedxaxa_000@mail.ru>
2026-02-16 00:03:08 +0300

    PUTmapping and first SNAPWHOT

--- reservation_system/src/main/java/com/example/reservation_system/controller/UserController.java
+++ reservation_system/src/main/java/com/example/reservation_system/controller/UserController.java
@@ -32,4 +32,14 @@ public class UserController {
     public void DeleteUser(@PathVariable Long id) {
         userService.DeleteUser(id);
     }
+
+    @PutMapping(path = "{id}")
+    public void UpdateUser(
+            @PathVariable Long id,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String name
+    ) {
+        userService.UpdateUser(id, email, name);
+    }
+
 }
--- reservation_system/src/main/java/com/example/reservation_system/service/UserService.java
+++ reservation_system/src/main/java/com/example/reservation_system/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.reservation_system.service;
 
 import com.example.reservation_system.repository.User;
 import com.example.reservation_system.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,6 +41,25 @@ public class UserService {
             throw new RuntimeException("User with id " + id + " does not exist");
         }
         userRepository.deleteById(id);
+    }
 
+    @Transactional
+    public void UpdateUser(Long id, String email, String name) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new RuntimeException("User with id " + id + " does not exist");
+        }
+        User user = optionalUser.get();
+        if (email != null && !email.equals(user.getEmail())) {
+            Optional<User> foundByEmail = userRepository.findByEmail(email);
+            if (foundByEmail.isPresent()) {
+                throw new RuntimeException("User with email " + user.getEmail() + " already exists");
+            }
+            user.setEmail(email);
+        }
+        if (name != null && !name.equals(user.getName())) {
+            user.setName(name);
+        }
+        //userRepository.save(user);
     }
 }
